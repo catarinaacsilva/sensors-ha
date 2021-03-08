123614f3d848b228600c8b1ca4ad80ff015c9228
Catarina <c.alexandracorreia@ua.pt>
2021-03-08 11:03:54 +0000

    Finish motion and smoke sensor

--- motion_smoke/mock_motion_smoke.py
+++ motion_smoke/mock_motion_smoke.py
@@ -47,7 +47,7 @@ def on_connect(mqttc, obj, flags, rc):
             msg = json.dumps({
                 'name': f"{t}_{i+1}",
                 'availability_topic': f'{CLIENT_ID}/status',  # Online, Offline
-                'device_class': 'motion' if t == 'motion' else 'smoke',
+                #'device_class': 'power_factor' if t == 'motion' else 'pressure',
                 'unit_of_measurement': '' if t == 'motion' else '',
                 'unique_id': f"{t}_{i+1}",
                 'state_topic': f'{t}_{i+1}/state',  # Value published
